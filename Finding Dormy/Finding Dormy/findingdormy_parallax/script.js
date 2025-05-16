84280bd11d23b219fc1938ff500ea800b6c7c3e9
Bon Roan R. Hernandez <brhernandez@gbox.adnu.edu.ph>
2025-05-16 22:54:23 +0800

    Delete Finding Dormy/Finding Dormy directory

--- Finding Dormy/Finding Dormy/findingdormy_parallax/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY; // Get the current scroll position
-
-  document.querySelectorAll('.layer').forEach(layer => {
-    const depth = parseFloat(layer.getAttribute('data-depth')); // Get the depth value
-    const movement = scrollY * depth; // Calculate movement based on depth
-    layer.style.transform = `translate3d(0, ${movement}px, 0)`; // Apply vertical movement
-  });
-});
